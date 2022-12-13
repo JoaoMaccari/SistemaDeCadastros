8e0a6cfa1a027487cc25712d0d52208c538626b2
João Elias Maccari <joaomaccari@hotmail.com>
2022-12-13 19:56:17 -0300

    att

--- SistemaCadastro/SistemaCadastro/Controllers/ContatoController.cs
+++ SistemaCadastro/SistemaCadastro/Controllers/ContatoController.cs
@@ -39,19 +39,64 @@ namespace SistemaCadastro.Controllers {
 
         public IActionResult Apagar(int id) {
 
-            _contatoRepositorio.Apagar(id);
-            return RedirectToAction("Index");
+            try {
+                bool apagado = _contatoRepositorio.Apagar(id);
+
+                if (apagado) {
+                    TempData["MensagemSucesso"] = "Contato apagado com sucesso.";
+                }
+                else {
+                    TempData["MensagemErro"] = $"Ops, não conseguimos apagar o contato.";
+                }
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception erro) {
+
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar o contato. Detalhe erro: {erro.Message}";
+                return RedirectToAction("index");
+            }
+
+            
         }
 
         [HttpPost]
         public IActionResult Criar(ContatoModel Contato) {
-            _contatoRepositorio.Adicionar(Contato);
-            return RedirectToAction("Index");
+
+            try {
+                if (ModelState.IsValid) {
+                    _contatoRepositorio.Adicionar(Contato);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso.";
+                    return RedirectToAction("Index");
+
+                }
+
+                return View(Contato);
+            }
+            catch (Exception erro) {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar o contato. Detalhe erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        
         }
 
         public IActionResult Alterar(ContatoModel contato) {
-            _contatoRepositorio.Atualizar(contato);
-            return RedirectToAction("Index");
+
+            try {
+                if (ModelState.IsValid) {
+                    _contatoRepositorio.Atualizar(contato);
+                    TempData["MensagemSucesso"] = "Contato alterado com sucesso.";
+                    return RedirectToAction("Index");
+                }
+
+                return View("Editar", contato);
+
+            }
+            catch (Exception erro) {
+                TempData["MensagemErro"] = $"Ops, erro ao editar o contato. Detalhe erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+            
         }
 
     }
--- SistemaCadastro/SistemaCadastro/Models/ContatoModel.cs
+++ SistemaCadastro/SistemaCadastro/Models/ContatoModel.cs
@@ -1,11 +1,19 @@
-﻿namespace SistemaCadastro.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCadastro.Models {
     public class ContatoModel {
 
         public int Id { get; set; }
 
-        public string Nome { get; set; } 
-        
+        [Required(ErrorMessage = "Digite o nome do contato")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o e-mail do contato")]
+        [EmailAddress(ErrorMessage ="O e-mail informado não é valido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o celular do contato")]
+        [Phone(ErrorMessage ="O ceulular informado não é valido")]
         public string Celular { get; set; }
     }
 }
